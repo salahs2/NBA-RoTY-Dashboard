44e0c42eecefc8719d6bc53f693c4f4e41cc38c3
salah salah <s.salahs1e@gmail.com>
2024-10-18 11:36:41 -0400

    implement get_data

---
+++ get_data.py
@@ -0,0 +1,72 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import numpy as np
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import players
+
+pd.set_option('display.max_columns', None)
+
+'''Getting list of rookies and their positions from tankathon.com'''
+class Data: 
+    def __init__(self, year : str):
+        self.year = year 
+        self.data = pd.DataFrame()
+    def get_rookies(self,):
+        # URL of the website to scrape
+        url = f'https://www.tankathon.com/past_drafts/{self.year}'
+
+        # Send an HTTP GET request to the URL
+        response = requests.get(url)
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(response.text, "html.parser")
+            soup_rookies = soup.find_all('div', 'mock-row-name')
+            
+            rookies = dict() # Key: Full Name Value: Draft Position
+            count = 1
+            for i in range(len(soup_rookies)):
+                temp = soup_rookies[i] 
+                if str(temp)[-20:-6] != 'Forfeited Pick':
+                    rookies.update({str(temp)[27:-6] : count})
+                    if type(count) is int and count < 58:
+                        count+=1
+                    else:
+                        count = "Undrafted"
+        return rookies
+    def save_data_to_csv(self,):
+        self.data.to_csv("nba_rookie_data", index=False)
+
+    def run(self): 
+        rookies = self.get_rookies()
+
+        '''Getting data from nba api'''
+        nba_players = players.get_players()
+
+        nba_rookies = []
+        frames = []
+        count = 0 
+        for player in nba_players:
+            if player["full_name"] in rookies:
+                nba_rookies.append(player)
+                #print(f"name: {player["full_name"]} id: {player['id']}")
+                try:   
+                    temp = playercareerstats.PlayerCareerStats(player_id=player['id']).get_data_frames()
+                    frames.append(temp[0])
+                    print(f"iteration: {count}")
+                    count+=1
+                    time.sleep(1)
+                except requests.Timeout:
+                    print("The request timed out")
+        self.data = pd.concat(frames)
+
+        self.data['PLAYER_ID'].map(rookies)
+        self.save_data_to_csv()
+
+data = Data(year='2023')
+
+data.run()
+
+print(data.df.head())
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import sqlite3 as sql
+import streamlit as st
 import pandas as pd
+import json
 
-# loads SQL magic into notebook
-%load_ext sql
-# %reload_ext sql
-# connects SQL magic notebook backend to the Basketball Dataset 
-%sql sqlite:///../input/basketball/basketball.sqlite 
+df = pd.read_csv("nba_rookie_data")
 
+print(df.head())
